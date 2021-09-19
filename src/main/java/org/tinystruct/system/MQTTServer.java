8cf366dde4c7a1c03f9c2f31896509a318c047ee
James <moverinfo@gmail.com>
2021-09-19 19:57:25 +0800

    Add MQTTServer.java

---
+++ src/main/java/org/tinystruct/system/MQTTServer.java
@@ -0,0 +1,26 @@
+package org.tinystruct.system;
+
+import org.tinystruct.AbstractApplication;
+import org.tinystruct.ApplicationException;
+
+public class MQTTServer extends AbstractApplication implements Bootstrap {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public String version() {
+        return null;
+    }
+
+    @Override
+    public void start() throws ApplicationException {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
