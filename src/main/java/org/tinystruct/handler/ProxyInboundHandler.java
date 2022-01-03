87fcd8eddd427d27bb1d18e2ea08cbd01a5f902a
James ZHOU <moverinfo@gmail.com>
2022-01-03 14:57:28 +0800

    Create Proxy handlers for inbound & outbound.

--- src/main/java/org/tinystruct/handler/ProxyInboundHandler.java
+++ src/main/java/org/tinystruct/handler/ProxyInboundHandler.java
@@ -18,28 +18,26 @@ package org.tinystruct.handler;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.*;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
 
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
+public abstract class ProxyInboundHandler extends ChannelInboundHandlerAdapter implements ProxyHandler {
 
-public abstract class ProxyFrontendHandler extends ChannelDuplexHandler implements ProxyHandler {
+    private static final InternalLogger logger = InternalLoggerFactory.getInstance(ProxyInboundHandler.class);
 
-    private final SocketAddress remoteAddress;
-
-    // As we use inboundChannel.eventLoop() when building the Bootstrap this does
-    // not need to be volatile as
-    // the outboundChannel will use the same EventLoop (and therefore Thread) as the
-    // inboundChannel.
     private Channel outboundChannel;
 
-    public ProxyFrontendHandler(String remoteHost, int remotePort) {
-        this.remoteAddress = new InetSocketAddress(remoteHost, remotePort);
+    String remoteHost = "localhost";
+    int remotePort;
+
+    public ProxyInboundHandler(String remoteHost, int remotePort) {
+        this.remoteHost = remoteHost;
+        this.remotePort = remotePort;
     }
 
-    public ProxyFrontendHandler(int remotePort) {
-        this.remoteAddress = new InetSocketAddress(remotePort);
+    public ProxyInboundHandler(int remotePort) {
+        this.remotePort = remotePort;
     }
 
     @Override
@@ -48,21 +46,13 @@ public abstract class ProxyFrontendHandler extends ChannelDuplexHandler implemen
 
         // Start the connection attempt.
         Bootstrap b = new Bootstrap();
-        b.group(inboundChannel.eventLoop()).channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel ch) throws Exception {
-                        ChannelHandler[] handlers;
-                        if ((handlers = initCodecs()) != null)
-                            addCodec(ch.pipeline(), handlers);
-                        addCodec(ch.pipeline(), new ProxyBackendHandler(inboundChannel));
-                    }
-                })
-                .option(ChannelOption.SO_KEEPALIVE, false)
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 500);
-
-        ChannelFuture f = b.connect(remoteAddress);
+        b.group(inboundChannel.eventLoop())
+                .channel(ctx.channel().getClass())
+                .handler(new ProxyOutboundHandler(inboundChannel))
+                .option(ChannelOption.AUTO_READ, false);
+        ChannelFuture f = b.connect(remoteHost, remotePort);
         outboundChannel = f.channel();
+        outboundChannel.pipeline().addLast(this.initCodecs());
         f.addListener(new ChannelFutureListener() {
             @Override
             public void operationComplete(ChannelFuture future) {
@@ -77,37 +67,25 @@ public abstract class ProxyFrontendHandler extends ChannelDuplexHandler implemen
         });
     }
 
-    public abstract ChannelHandler[] initCodecs();
-
     @Override
-    public void channelInactive(ChannelHandlerContext ctx) {
-        if (outboundChannel != null) {
-            closeOnFlush(outboundChannel);
-        }
-    }
+    public void channelRead(final ChannelHandlerContext ctx, Object msg) {
+        logger.info("{}", msg.getClass());
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        System.err.println("Errors occured in Frontend.");
-        cause.printStackTrace(System.err);
-        closeOnFlush(ctx.channel());
-    }
+        if (outboundChannel.isActive() && outboundChannel.isWritable()) {
+            if (msg instanceof HttpRequest) {
+                HttpRequest req = (HttpRequest) msg;
+                req.headers().set(HttpHeaderNames.HOST, this.remoteHost + ":" + this.remotePort);
+                logger.info("{}", req);
+            } else if (msg instanceof HttpContent) {
+                HttpContent req = (HttpContent) msg;
+                logger.info("{}", req);
+            }
 
-    /**
-     * Closes the specified channel after all queued write requests are flushed.
-     */
-    static void closeOnFlush(Channel ch) {
-        if (ch.isActive()) {
-            ch.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
-        }
-    }
-
-    @Override
-    public void channelRead(final ChannelHandlerContext ctx, Object msg) throws Exception {
-        if (outboundChannel.isActive()) {
-            outboundChannel.writeAndFlush(msg).addListener(new ChannelFutureListener() {
+            outboundChannel.writeAndFlush(msg)
+                    .addListener(new ChannelFutureListener() {
                 @Override
                 public void operationComplete(ChannelFuture future) {
+                    logger.info("{}", future.isDone());
                     if (future.isSuccess()) {
                         // was able to flush out data, start to read the next chunk
                         ctx.channel().read();
@@ -119,20 +97,30 @@ public abstract class ProxyFrontendHandler extends ChannelDuplexHandler implemen
         }
     }
 
-    /**
-     * Calls {@link ChannelHandlerContext#write(Object, ChannelPromise)} to forward
-     * to the next {@link ChannelOutboundHandler} in the {@link ChannelPipeline}.
-     * <p>
-     * Sub-classes may override this method to change behavior. Convert the command
-     * line to be RedisMessage types.
-     */
     @Override
-    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
-        ctx.write(msg, promise);
+    public void channelInactive(ChannelHandlerContext ctx) {
+        if (outboundChannel != null) {
+            closeOnFlush(outboundChannel);
+        }
     }
 
     @Override
-    public void addCodec(ChannelPipeline pipeline, ChannelHandler... handlers) {
-        pipeline.addLast(handlers);
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        closeOnFlush(ctx.channel());
+    }
+
+    @Override
+    public void addCodec(ChannelPipeline pipeline, ChannelHandler... handler) {
+        pipeline.addLast(handler);
+    }
+
+    /**
+     * Closes the specified channel after all queued write requests are flushed.
+     */
+    static void closeOnFlush(Channel ch) {
+        if (ch.isActive()) {
+            ch.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+        }
     }
 }
