690ffcc8741650cee251bdf5489c2ca5acb6d7eb
James ZHOU <moverinfo@gmail.com>
2022-05-09 22:20:27 +0800

    Update the version to 0.4.0.

--- src/main/java/org/tinystruct/system/ApplicationManager.java
+++ src/main/java/org/tinystruct/system/ApplicationManager.java
@@ -36,7 +36,7 @@ public final class ApplicationManager {
     private static final Actions actions = Actions.getInstance();
     private static Configuration<String> settings;
     private static volatile boolean initialized = false;
-    public static final String VERSION = "0.3.9";
+    public static final String VERSION = "0.4.0";
     private static final boolean WINDOWS = System.getProperty("os.name").startsWith("WIN");
 
     private ApplicationManager() {
