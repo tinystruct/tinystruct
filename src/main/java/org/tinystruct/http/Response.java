af0aef5cc7fa3ad24d89a171839d1537bb7ba967
James ZHOU <moverinfo@gmail.com>
2022-06-06 23:30:38 +0800

    Refine the Response interface.

--- src/main/java/org/tinystruct/http/Response.java
+++ src/main/java/org/tinystruct/http/Response.java
@@ -1,6 +1,7 @@
 package org.tinystruct.http;
 
-import java.util.List;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public interface Response extends Protocol {
     /**
@@ -18,5 +19,11 @@ public interface Response extends Protocol {
     /**
      * Returns the headers of this message.
      */
-    List<Header> headers();
+    Headers headers();
+
+    void addHeader(String header, String value);
+
+    PrintWriter getWriter();
+
+    void sendRedirect(String url) throws IOException;
 }
