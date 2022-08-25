e0a41dba9da93e7987ffa24a4f7552a1cd75b4c0
James ZHOU <moverinfo@gmail.com>
2022-08-26 00:52:53 +0800

    Add constants for keys.

---
+++ src/main/java/org/tinystruct/http/Constants.java
@@ -0,0 +1,12 @@
+package org.tinystruct.http;
+
+/**
+ * Constants for keys.
+ */
+public interface Constants {
+    String HTTP_REQUEST = "HTTP_REQUEST";
+    String HTTP_RESPONSE = "HTTP_RESPONSE";
+    String HTTP_SCHEME = "HTTP_SCHEME";
+    String HTTP_SERVER = "HTTP_SERVER";
+    String HTTP_HOST = "HTTP_HOST";
+}
