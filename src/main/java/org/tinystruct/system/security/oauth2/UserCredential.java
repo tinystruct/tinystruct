cf2b3c6fdd34dfc55d930049dd1d9afb8b9fcfa9
James ZHOU <moverinfo@gmail.com>
2022-10-04 20:38:05 +0800

    Make sure input stream is closed.

--- src/main/java/org/tinystruct/system/security/oauth2/UserCredential.java
+++ src/main/java/org/tinystruct/system/security/oauth2/UserCredential.java
@@ -5,12 +5,20 @@ import org.tinystruct.data.component.Builder;
 import org.tinystruct.system.security.Credential;
 import org.tinystruct.system.util.TextFileLoader;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class UserCredential implements Credential {
 
     @Override
     public String get(String key) throws ApplicationException {
         TextFileLoader loader = new TextFileLoader();
-        loader.setInputStream(UserCredential.class.getResourceAsStream("/clients_secrets.json"));
+        try (InputStream stream = UserCredential.class.getResourceAsStream("/clients_secrets.json")) {
+            loader.setInputStream(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         Builder builder = new Builder();
         builder.parse(loader.getContent().toString());
 
