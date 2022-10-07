c9b866003c0cdceb7ec43649489561976c6826ff
James ZHOU <moverinfo@gmail.com>
2022-10-07 13:09:47 +0800

    Add one more case to test the functionality of the DistributedLock.

--- src/test/java/org/tinystruct/valve/DistributedLockTests.java
+++ src/test/java/org/tinystruct/valve/DistributedLockTests.java
@@ -8,7 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tinystruct.ApplicationException;
 
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,6 +18,7 @@ public class DistributedLockTests {
     private static final Logger log = LoggerFactory.getLogger(DistributedLockTests.class);
     private static int tickets = 100;
     private static CountDownLatch latch;
+    private static long n;
 
     @AfterAll
     static void done() {
@@ -31,7 +34,7 @@ public class DistributedLockTests {
 
     @Test
     public void test() throws InterruptedException {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 200; i++) {
             new Thread(new ticket(), "Window #" + i).start();
         }
     }
@@ -39,11 +42,11 @@ public class DistributedLockTests {
     @BeforeEach
     public void setUp() throws ApplicationException {
         latch = new CountDownLatch(tickets);
+        n = 0L;
     }
 
     @AfterEach
     void tearDown() {
-
     }
 
     @Test
@@ -52,15 +55,32 @@ public class DistributedLockTests {
         try {
             lock.lock();
             System.out.println("Printed after locked.");
-        } catch (ApplicationException e) {
-            e.printStackTrace();
         } finally {
-            try {
-                lock.unlock();
-            } catch (ApplicationException e) {
-                e.printStackTrace();
-            }
+            lock.unlock();
         }
+    }
+
+    @Test
+    void testLock2() throws InterruptedException {
+        final Lock lock = new DistributedLock("7439e9a6-0828-422f-8c86-3f9b4f7e1460".getBytes(StandardCharsets.UTF_8));
+//        final ReentrantLock lock = new ReentrantLock();
+        n = 0L;
+        Thread[] threads = new Thread[100];
+        CountDownLatch latch = new CountDownLatch(threads.length);
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    lock.lock();
+                    n++;
+                    lock.unlock();
+                }
+                latch.countDown();
+            });
+            threads[i].start();
+        }
+
+        latch.await();
+        assertEquals(10000, n);
     }
 
     static class ticket implements Runnable {
@@ -79,14 +99,8 @@ public class DistributedLockTests {
                         // TODO
                         log.info(Thread.currentThread().getName() + " is selling #" + (tickets--) + " with Lock#" + lock.id());
                     latch.countDown();
-                } catch (ApplicationException e) {
-                    e.printStackTrace();
                 } finally {
-                    try {
-                        lock.unlock();
-                    } catch (ApplicationException e) {
-                        e.printStackTrace();
-                    }
+                    lock.unlock();
                 }
             }
         }
