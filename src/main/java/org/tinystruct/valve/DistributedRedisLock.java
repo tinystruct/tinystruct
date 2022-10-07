56b33dfeb9d919a9b839cca0352a93e82d6ad766
James ZHOU <moverinfo@gmail.com>
2022-10-07 13:07:58 +0800

    Update implementation of the Lock with redis.

--- src/main/java/org/tinystruct/valve/DistributedRedisLock.java
+++ src/main/java/org/tinystruct/valve/DistributedRedisLock.java
@@ -5,6 +5,7 @@ import io.lettuce.core.RedisURI;
 import io.lettuce.core.ScriptOutputType;
 import io.lettuce.core.api.StatefulRedisConnection;
 import org.tinystruct.ApplicationException;
+import org.tinystruct.ApplicationRuntimeException;
 import org.tinystruct.system.Configuration;
 
 import java.io.IOException;
@@ -27,12 +28,32 @@ import java.util.logging.Logger;
  */
 public class DistributedRedisLock implements Lock {
     private static final Logger logger = Logger.getLogger(DistributedRedisLock.class.getName());
+    private static String lockScript;
+    private static String unlockScript;
+
+    static {
+        ByteBuffer buff;
+        try (InputStream stream = Objects.requireNonNull(DistributedRedisLock.class.getResource("/lock.lua")).openStream(); ReadableByteChannel channel = Channels.newChannel(stream)) {
+            buff = ByteBuffer.allocate(800);
+            channel.read(buff);
+            lockScript = new String(buff.array(), StandardCharsets.UTF_8).trim();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Load lock.lua error.", e);
+        }
+
+        try (InputStream stream = Objects.requireNonNull(DistributedRedisLock.class.getResource("/unlock.lua")).openStream(); ReadableByteChannel channel = Channels.newChannel(stream);) {
+            buff = ByteBuffer.allocate(1024);
+            channel.read(buff);
+            unlockScript = new String(buff.array(), StandardCharsets.UTF_8).trim();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Load unlock.lua error.", e);
+        }
+    }
+
     private final RedisURI uri;
     private final StatefulRedisConnection<String, String> redisClient;
     private String id;
     private String value;
-    private static String lockScript;
-    private static String unlockScript;
 
     public DistributedRedisLock() {
         this(UUID.randomUUID().toString());
@@ -53,8 +74,8 @@ public class DistributedRedisLock implements Lock {
                 .withDatabase(1)
                 .withTimeout(Duration.ofSeconds(60)).build();
 
-        if (settings.get("redis.password")!=null)
-                this.uri.setPassword(settings.get("redis.password").toCharArray());
+        if (settings.get("redis.password") != null)
+            this.uri.setPassword(settings.get("redis.password").toCharArray());
 
         this.redisClient = RedisClient.create(this.uri).connect();
         this.id = UUID.randomUUID().toString();
@@ -66,38 +87,19 @@ public class DistributedRedisLock implements Lock {
         this.id = id;
     }
 
-    static {
-        ByteBuffer buff;
-        try (InputStream stream = Objects.requireNonNull(DistributedRedisLock.class.getResource("/lock.lua")).openStream(); ReadableByteChannel channel = Channels.newChannel(stream)){
-            buff = ByteBuffer.allocate(800);
-            channel.read(buff);
-            lockScript = new String(buff.array(), StandardCharsets.UTF_8).trim();
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Load lock.lua error.", e);
-        }
-
-        try (InputStream stream = Objects.requireNonNull(DistributedRedisLock.class.getResource("/unlock.lua")).openStream(); ReadableByteChannel channel = Channels.newChannel(stream);){
-            buff = ByteBuffer.allocate(1024);
-            channel.read(buff);
-            unlockScript = new String(buff.array(), StandardCharsets.UTF_8).trim();
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Load unlock.lua error.", e);
-        }
-    }
-
     @Override
-    public void lock() throws ApplicationException {
+    public void lock() {
         while ((Long) (this.redisClient.sync().eval(lockScript, ScriptOutputType.INTEGER, new String[]{this.id, this.value}, "1000")) == 0) {
             try {
                 Thread.sleep(1);
             } catch (InterruptedException e) {
-
+                throw new ApplicationRuntimeException(e.getMessage(), e.getCause());
             }
         }
     }
 
     @Override
-    public boolean tryLock() throws ApplicationException {
+    public boolean tryLock() {
         return this.redisClient.sync().eval(lockScript, ScriptOutputType.INTEGER, new String[]{this.id, this.value}, "1000");
     }
 
@@ -107,7 +109,7 @@ public class DistributedRedisLock implements Lock {
     }
 
     @Override
-    public void unlock() throws ApplicationException {
+    public void unlock() {
         this.redisClient.sync().eval(unlockScript, ScriptOutputType.INTEGER, this.id, this.value);
     }
 
