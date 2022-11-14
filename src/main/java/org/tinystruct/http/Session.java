1cdaa81450e6f27cf9fbc735fa1476405d4f6d3c
James ZHOU <moverinfo@gmail.com>
2022-11-15 00:29:43 +0800

    Add isExpired method.

--- src/main/java/org/tinystruct/http/Session.java
+++ src/main/java/org/tinystruct/http/Session.java
@@ -1,8 +1,12 @@
 package org.tinystruct.http;
 
+import org.apache.catalina.SessionListener;
+
 public interface Session {
     String getId();
     void setAttribute(String key, Object value);
     Object getAttribute(String key);
     void removeAttribute(String key);
+
+    boolean isExpired();
 }
