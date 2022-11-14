f527676bbd056966ab758b535bf06ded2a4e8ec3
James ZHOU <moverinfo@gmail.com>
2022-11-15 00:41:58 +0800

    Create SessionEvent and Type of event.

--- src/main/java/org/tinystruct/http/SessionEvent.java
+++ src/main/java/org/tinystruct/http/SessionEvent.java
@@ -3,15 +3,35 @@ package org.tinystruct.http;
 import java.util.EventObject;
 
 public class SessionEvent extends EventObject {
+
+    private Type type;
+
     /**
      * Constructs a prototypical Event.
      *
+     * @param type Type of event
      * @param source the object on which the Event initially occurred
      * @throws IllegalArgumentException if source is null
      */
-    public SessionEvent(Object source) {
+    public SessionEvent(Type type, Session source) {
         super(source);
+        this.type = type;
     }
 
+    public Session getSession() {
+        return (Session) source;
+    }
+
+    public Type getType() {
+        return this.type;
+    }
+
+    /**
+     * Event type.
+     */
+    public enum Type {
+        CREATED,
+        DESTROYED
+    }
     //TODO
 }
