18e8b4234e8a944411034843644f6b8fea5bd0b1
James ZHOU <moverinfo@gmail.com>
2022-11-15 00:42:37 +0800

    Implement Monitor interface.

--- src/main/java/org/tinystruct/http/SessionManager.java
+++ src/main/java/org/tinystruct/http/SessionManager.java
@@ -1,19 +1,36 @@
 package org.tinystruct.http;
 
+import org.tinystruct.system.scheduling.Scheduler;
+import org.tinystruct.system.scheduling.TimeIterator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 
-public final class SessionManager {
-    private final ConcurrentHashMap<String, Session> sessions = new ConcurrentHashMap<String, Session>();
+public final class SessionManager implements Monitor<SessionListener> {
+    private static final ConcurrentHashMap<String, Session> sessions = new ConcurrentHashMap<String, Session>();
+    private transient final List<SessionListener> listeners = new ArrayList<>();
 
     private SessionManager() {
+        Scheduler.getInstance().schedule(new ExpirationValidator(), new TimeIterator(0, 0, 0), 1000);
     }
 
     public static SessionManager getInstance() {
         return SingletonHolder.manager;
     }
 
-    public void setSession(String sessionId, Session value) {
-        sessions.put(sessionId, value);
+    public void setSession(String sessionId, Session session) {
+        sessions.put(sessionId, session);
+        SessionEvent event = new SessionEvent(SessionEvent.Type.CREATED, session);
+        fireEvent(event);
+    }
+
+    private void fireEvent(SessionEvent event) {
+        if (listeners.size() < 1) {
+            return;
+        }
+        listeners.forEach(listener -> listener.onSessionEvent(event));
     }
 
     public Session getSession(String sessionId) {
@@ -21,7 +38,25 @@ public final class SessionManager {
     }
 
     public void removeSession(String sessionId) {
-        sessions.remove(sessionId);
+        Session session = sessions.remove(sessionId);
+        SessionEvent event = new SessionEvent(SessionEvent.Type.DESTROYED, session);
+        listeners.forEach(listener -> listener.onSessionEvent(event));
+    }
+
+    @Override
+    public void addListener(SessionListener listener) {
+        this.listeners.add(listener);
+    }
+
+    private static final class ExpirationValidator extends TimerTask {
+        @Override
+        public void run() {
+            sessions.forEach((sessionId, session) -> {
+                if(session.isExpired()) {
+                    getInstance().removeSession(sessionId);
+                }
+            });
+        }
     }
 
     private static final class SingletonHolder {
