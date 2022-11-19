bdaabf42599941073d6bdf0562d2bf153d7f6c1c
James ZHOU <moverinfo@gmail.com>
2022-11-19 22:03:34 +0800

    Implement new method & use parseQuery to set the parameters.

--- src/main/java/org/tinystruct/http/RequestBuilder.java
+++ src/main/java/org/tinystruct/http/RequestBuilder.java
@@ -6,14 +6,13 @@ import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.QueryStringDecoder;
 import io.netty.handler.codec.http.cookie.ServerCookieDecoder;
-import io.netty.handler.codec.http.multipart.DefaultHttpDataFactory;
-import io.netty.handler.codec.http.multipart.HttpDataFactory;
-import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
-import io.netty.handler.codec.http.multipart.InterfaceHttpData;
+import io.netty.handler.codec.http.multipart.*;
 import io.netty.util.CharsetUtil;
 import org.apache.catalina.util.StandardSessionIdGenerator;
+import org.tinystruct.data.Attachment;
 import org.tinystruct.data.FileEntity;
 
+import java.io.IOException;
 import java.util.*;
 
 public class RequestBuilder extends RequestWrapper<FullHttpRequest> {
@@ -21,7 +20,7 @@ public class RequestBuilder extends RequestWrapper<FullHttpRequest> {
     private final Headers headers = new Headers();
     private final Cookie[] cookies;
     private final HashMap<String, List<String>> params = new HashMap<>();
-    private List<FileEntity> attachments = new ArrayList<>();
+    private List<FileEntity> attachments;
     private String query;
     private Version version;
     private Method method;
@@ -61,17 +60,30 @@ public class RequestBuilder extends RequestWrapper<FullHttpRequest> {
 
         if (content.capacity() > 0) {
             String requestBody;
-            String[] args, pair;
-            switch (this.headers.get(Header.CONTENT_TYPE).toString()) {
+            String contentType = null;
+            Object originHeader;
+            if ((originHeader = this.headers.get(Header.CONTENT_TYPE)) != null && (contentType = originHeader.toString()).indexOf(';') != -1) {
+                contentType = contentType.substring(0, contentType.indexOf(';'));
+            }
+
+            switch (Objects.requireNonNull(contentType)) {
                 case "multipart/form-data":
                     requestBody = content.toString(CharsetUtil.UTF_8);
-                    args = requestBody.split("&");
-                    for (i = 0; i < args.length; i++) {
-                        if (args[i].contains("=")) {
-                            pair = args[i].split("=");
-                            this.setParameter(pair[0], List.of(pair[1]));
+                    String boundary = null;
+                    int index;
+                    if ((index = originHeader.toString().lastIndexOf("boundary=")) != -1) {
+                        boundary = "--" + originHeader.toString().substring(index + 9);
+                        if (boundary.endsWith("\n")) {
+                            //strip it off
+                            boundary = boundary.substring(0, boundary.length() - 1);
                         }
+                        requestBody = requestBody.substring(0, requestBody.indexOf(boundary) + boundary.length());
                     }
+
+                    if (boundary != null && !requestBody.startsWith(boundary)) {
+                        parseQuery(requestBody, false);
+                    }
+
                     // TODO
                     final HttpDataFactory factory = new DefaultHttpDataFactory(DefaultHttpDataFactory.MINSIZE); // Disk if size exceed
                     HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(factory, request);
@@ -80,33 +92,28 @@ public class RequestBuilder extends RequestWrapper<FullHttpRequest> {
                     while (decoder.hasNext()) {
                         fileData = decoder.next();
                         if (fileData != null && fileData.getHttpDataType() == InterfaceHttpData.HttpDataType.FileUpload) {
-                            list.add((FileEntity) fileData);
-                        }
-                    }
-                    this.attach(list);
-                    break;
-                case "application/x-www-form-urlencoded; charset=UTF-8":
-                    requestBody = java.net.URLDecoder.decode(content.toString(CharsetUtil.UTF_8), CharsetUtil.UTF_8);
-                    args = requestBody.split("&");
-                    for (i = 0; i < args.length; i++) {
-                        if (args[i].contains("=")) {
-                            pair = args[i].split("=");
-                            this.setParameter(pair[0], List.of(pair[1]));
-                        }
-                    }
-                    break;
-                case "text/plain; charset=UTF-8":
-                case "application/json":
-                default:
-                    requestBody = content.toString(CharsetUtil.UTF_8);
-                    args = requestBody.split("&");
-                    for (i = 0; i < args.length; i++) {
-                        if (args[i].contains("=")) {
-                            pair = args[i].split("=");
-                            this.setParameter(pair[0], List.of(pair[1]));
-                        }
-                    }
+                            MixedFileUpload upload = (MixedFileUpload) fileData;
+                            Attachment attachment = new Attachment();
+                            attachment.setContentType(upload.getContentType());
+                            attachment.setFilename(upload.getFilename());
+                            attachment.setContentTransferEncoding(upload.getContentTransferEncoding());
+                            try {
+                                attachment.setContent(upload.get());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
 
+                            list.add(attachment);
+                        }
+                    }
+                    this.attachments = list;
+                    break;
+
+                case "text/plain": break;
+                case "application/json": break;
+                case "application/x-www-form-urlencoded":
+                default:
+                    parseQuery(content.toString(CharsetUtil.UTF_8), false);
                     break;
             }
         }
@@ -121,12 +128,9 @@ public class RequestBuilder extends RequestWrapper<FullHttpRequest> {
         }
     }
 
-    private void attach(List<FileEntity> list) {
-        this.attachments = list;
-    }
-
+    @Override
     public List<FileEntity> getAttachments() {
-        return this.attachments;
+        return this.attachments != null ? this.attachments : null;
     }
 
     private QueryStringDecoder parseQuery(String uri, boolean hasPath) {
