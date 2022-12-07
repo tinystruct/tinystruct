9f7c17a3dcbb719f4d9446d563fda57fa780999d
James ZHOU <moverinfo@gmail.com>
2022-12-08 00:03:31 +0800

    Update DistributedHashMap.java

--- src/main/java/org/tinystruct/valve/DistributedHashMap.java
+++ src/main/java/org/tinystruct/valve/DistributedHashMap.java
@@ -1,20 +1,13 @@
 package org.tinystruct.valve;
 
-import org.tinystruct.ApplicationException;
-import org.tinystruct.valve.DistributedLock;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.channels.FileLock;
-import java.nio.file.Paths;
-import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * DistributedHashMap implementation. NOT COMPLETE.
+ * DistributedHashMap implementation. TO BE COMPLETE.
  *
  * @param <T> Data type
  */
@@ -41,100 +34,5 @@ public class DistributedHashMap<T> extends ConcurrentHashMap<String, Queue<T>> {
         }
     }
 
-    @Override
-    public Queue<T> get(Object key) {
-        try {
-            Queue<T> queue = new ArrayDeque<T>();
-            FileLock fileLock;
-            this.data = new RandomAccessFile("." + key, "rw");
-            if (this.data.length() >= FIXED_LOCK_DATA_SIZE) {
-                this.size = (int) this.data.length() / FIXED_LOCK_DATA_SIZE;
-                fileLock = data.getChannel().tryLock(0L, Long.MAX_VALUE, false);
-
-                if (fileLock != null) {
-                    if (this.size > 0) {
-                        data.seek(0);
-
-                    }
-                }
-            } else {
-                this.size = 0;
-                if (queue.size() > 0) {
-
-                }
-            }
-
-
-        } catch (Exception e) {
-
-        }
-        return super.get(key);
-    }
-
-    @Override
-    public Queue<T> put(String key, Queue<T> value) {
-        FileLock fileLock = null;
-        try {
-            this.lock.lock();
-
-            this.data = new RandomAccessFile(this.hash, "rw");
-            if (!this.contains(key)) {
-                fileLock = data.getChannel().tryLock();
-
-                long length = data.length();
-                if (null != fileLock) {
-                    if (length >= FIXED_LOCK_DATA_SIZE) {
-                        this.size = (int) (length / FIXED_LOCK_DATA_SIZE);
-                    } else
-                        this.size = 0;
-
-                    if (this.size > 0) {
-                        data.seek(this.size * FIXED_LOCK_DATA_SIZE);
-                        data.writeBytes(key);
-                        data.writeLong(1L);
-                    }
-
-                    value = super.put(key, value);
-                }
-            }
-            this.lock.unlock();
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } finally {
-            try {
-                if (fileLock != null) {
-                    fileLock.release();
-                    data.close();
-                    data.getChannel().close();
-                }
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-
-        return value;
-
-    }
-
-    @Override
-    public Queue<T> remove(Object key) {
-        this.lock.lock();
-        Paths.get(this.hash).toFile().delete();
-        super.remove(key);
-        this.lock.unlock();
-
-        return null;
-    }
-
-    @Override
-    public Set<Entry<String, Queue<T>>> entrySet() {
-        // TODO Auto-generated method stub
-        return super.entrySet();
-    }
 
 }
