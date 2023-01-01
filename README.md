96457a135fe3c76f9794338df5476afe402bbc6f
James ZHOU <moverinfo@gmail.com>
2023-01-01 23:39:30 +0800

    Update the version to be 0.8.6.

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Installation and Getting Started
 <dependency>
   <groupId>org.tinystruct</groupId>
   <artifactId>tinystruct</artifactId>
-  <version>0.8.5</version>
+  <version>0.8.6</version>
   <classifier>jar-with-dependencies</classifier>
 </dependency>
 ```
@@ -67,7 +67,7 @@ Execute in CLI mode
 $ bin/dispatcher --version
 
   _/  '         _ _/  _     _ _/
-  /  /  /) (/ _)  /  /  (/ (  /  0.8.5
+  /  /  /) (/ _)  /  /  (/ (  /  0.8.6
            /
 ```
 ```tcsh
--- build.sh
+++ build.sh
@@ -3,26 +3,14 @@ set -ex
 
 ./mvnw clean install
 source ~/.bash_profile
-$JAVA_HOME/bin/native-image --no-fallback \
-                        --initialize-at-run-time=io.netty.handler.ssl.BouncyCastleAlpnSslUtils \
-                        --initialize-at-run-time=io.netty.util.AbstractReferenceCounted \
-                        --initialize-at-run-time=io.netty.channel.epoll.Epoll,io.netty.channel.epoll.EpollEventLoop,io.netty.channel.epoll.Native,io.netty.channel.epoll.EpollEventArray,io.netty.channel.DefaultFileRegion,io.netty.channel.unix.Errors \
-                        --initialize-at-build-time=org.slf4j.MDC \
-                        --initialize-at-build-time=ch.qos.logback.classic.Level \
-                        --initialize-at-build-time=ch.qos.logback.classic.Logger \
-                        --initialize-at-build-time=ch.qos.logback.core.util.StatusPrinter \
-                        --initialize-at-build-time=ch.qos.logback.core.status.StatusBase \
-                        --initialize-at-build-time=ch.qos.logback.core.status.InfoStatus \
-                        --initialize-at-build-time=ch.qos.logback.core.spi.AppenderAttachableImpl \
-                        --initialize-at-build-time=org.slf4j.LoggerFactory \
-                        --initialize-at-build-time=ch.qos.logback.core.util.Loader \
-                        --initialize-at-build-time=org.slf4j.impl.StaticLoggerBinder \
-                        --initialize-at-build-time=ch.qos.logback.classic.spi.ThrowableProxy \
-                        --initialize-at-build-time=ch.qos.logback.core.CoreConstants \
-                        --report-unsupported-elements-at-runtime \
-                        --add-exports=org.graalvm.nativeimage.builder/com.oracle.svm.core=ALL-UNNAMED \
-                        --add-opens=java.base/jdk.internal.misc=ALL-UNNAMED \
--cp ./target/tinystruct-0.8.5-jar-with-dependencies.jar \
+
+$GRAALVM_HOME/bin/native-image -H:ConfigurationFileDirectories=/Volumes/Untitled/development/tinystruct/bin/.metadata --no-fallback \
+--initialize-at-run-time=org.slf4j.LoggerFactory \
+--initialize-at-run-time=io.netty.handler.ssl.BouncyCastleAlpnSslUtils \
+--initialize-at-run-time=io.netty.util.AbstractReferenceCounted \
+--trace-class-initialization=org.slf4j.LoggerFactory \
+--add-opens=java.base/jdk.internal.misc=ALL-UNNAMED \
+-cp ./target/tinystruct-0.8.6-jar-with-dependencies.jar \
 -H:Name=dispatcher-native \
 -H:Class=org.tinystruct.system.Dispatcher \
 -H:+ReportExceptionStackTraces \
