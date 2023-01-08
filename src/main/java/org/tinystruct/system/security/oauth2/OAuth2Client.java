bc75787c36864c840e06c137470998ac189e5c74
James ZHOU <moverinfo@gmail.com>
2023-01-08 21:11:16 +0800

    Update OAuth2Client.java due to the update for URLRequest.

--- src/main/java/org/tinystruct/system/security/oauth2/OAuth2Client.java
+++ src/main/java/org/tinystruct/system/security/oauth2/OAuth2Client.java
@@ -2,11 +2,10 @@ package org.tinystruct.system.security.oauth2;
 
 import org.tinystruct.ApplicationException;
 import org.tinystruct.data.component.Builder;
-import org.tinystruct.http.URLRequest;
+import org.tinystruct.http.*;
 import org.tinystruct.system.security.Authentication;
 import org.tinystruct.system.security.Credential;
 
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -19,24 +18,24 @@ public class OAuth2Client implements Authentication {
     boolean status;
     Object token;
 
-    public enum GRANT_TYPE {
-        client_credentials, password, authorization_code, refresh_token, token
-    }
-
     @Override
     public void identify(Credential credential, Map<String, Object> parameters) throws ApplicationException {
         try {
             URL uri = new URL(credential.get("token_uri"));
-            Map<String, String> headers = new HashMap<String, String>();
-            headers.put("Content-Type", "application/x-www-form-urlencoded");
-            headers.put("cache-control", "no-cache");
+            Headers headers = new Headers();
+            headers.add(Header.CONTENT_TYPE.set("application/x-www-form-urlencoded"));
+            headers.add(Header.CACHE_CONTROL.set("no-cache"));
 
             String userCredentials = credential.get("client_id") + ":" + credential.get("client_secret");
             String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userCredentials.getBytes()));
-            headers.put("Authorization", basicAuth);
+            headers.add(Header.AUTHORIZATION.set(basicAuth));
+
+            HttpRequestBuilder builder = new HttpRequestBuilder();
+            builder.setMethod(Method.GET).setHeaders(headers);
+            parameters.forEach(builder::setParameter);
 
             URLRequest request = new URLRequest(uri);
-            String resp = new String(request.setHeaders(headers).send(parameters), StandardCharsets.UTF_8);
+            String resp = new String(request.send(builder), StandardCharsets.UTF_8);
             Builder struct = new Builder();
             struct.parse(resp);
             if ((this.token = struct.get("access_token")) != null) {
@@ -63,19 +62,28 @@ public class OAuth2Client implements Authentication {
     public byte[] resource(URL url, Map<String, Object> parameters) {
         String bearer = "Bearer " + token;
 
-        Map<String, String> headers = new HashMap<String, String>();
-        headers.put("Content-Type", "application/x-www-form-urlencoded");
-        headers.put("Authorization", bearer);
-        headers.put("cache-control", "no-cache");
+        Headers headers = new Headers();
+        headers.add(Header.CONTENT_TYPE.set("application/x-www-form-urlencoded"));
+        headers.add(Header.CACHE_CONTROL.set("no-cache"));
+        headers.add(Header.AUTHORIZATION.set(bearer));
+
         URLRequest request = new URLRequest(url);
+        HttpRequestBuilder builder = new HttpRequestBuilder();
+        builder.setMethod(Method.GET).setHeaders(headers);
+        parameters.forEach(builder::setParameter);
+
         try {
-            return request.setHeaders(headers).send(parameters);
+            return request.send(builder);
         } catch (ApplicationException e) {
             e.printStackTrace();
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public enum GRANT_TYPE {
+        client_credentials, password, authorization_code, refresh_token, token
     }
 
 }
