69c968faac60211a0cb733b928b130af348e7cfe
James ZHOU <moverinfo@gmail.com>
2023-06-11 18:28:28 +0800

    Add MQTT server.

--- src/main/java/org/tinystruct/system/MQTTServer.java
+++ src/main/java/org/tinystruct/system/MQTTServer.java
@@ -1,9 +1,21 @@
 package org.tinystruct.system;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.mqtt.MqttDecoder;
+import io.netty.handler.codec.mqtt.MqttEncoder;
 import org.tinystruct.AbstractApplication;
 import org.tinystruct.ApplicationException;
+import org.tinystruct.handler.MQTTServerHandler;
 
 public class MQTTServer extends AbstractApplication implements Bootstrap {
+
     @Override
     public void init() {
 
@@ -16,6 +28,32 @@ public class MQTTServer extends AbstractApplication implements Bootstrap {
 
     @Override
     public void start() throws ApplicationException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup);
+            b.option(ChannelOption.SO_BACKLOG, 1024);
+            b.channel(NioServerSocketChannel.class);
+            b.childHandler(new ChannelInitializer<SocketChannel>() {
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast("decoder", new MqttDecoder());
+                    ch.pipeline().addLast("encoder", MqttEncoder.INSTANCE);
+                    ch.pipeline().addLast("handler", MQTTServerHandler.INSTANCE);
+                }
+            });
+
+            ChannelFuture f = b.bind(1883).sync();
+            System.out.println("MQTT Broker initiated...");
+
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
 
     }
 
