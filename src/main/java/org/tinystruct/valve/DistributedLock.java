d8b2234d5bbbbf9f235ab66caa1554a57ec0e6d9
James ZHOU <moverinfo@gmail.com>
2024-01-13 15:07:41 +0800

    Add some code comments.

--- src/main/java/org/tinystruct/valve/DistributedLock.java
+++ src/main/java/org/tinystruct/valve/DistributedLock.java
@@ -3,11 +3,13 @@ package org.tinystruct.valve;
 import org.tinystruct.ApplicationException;
 import org.tinystruct.ApplicationRuntimeException;
 
+import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 /**
- * Distributed lock depends on File system.
+ * Distributed lock implementation based on the File system.
  * Usage:
  * <pre>
  * {@code
@@ -15,8 +17,8 @@ import java.util.concurrent.TimeUnit;
  * ...
  * try {
  *    lock.lock();
- *    // TODO
- *    logger.info(Thread.currentThread().getName() + " is selling #" + (tickets--) + " with Lock#" + lock.id());
+ *    // Critical section
+ *    logger.info(Thread.currentThread().getName() + " is performing a task with Lock#" + lock.id());
  * } finally {
  *    lock.unlock();
  * }
@@ -27,38 +29,69 @@ import java.util.concurrent.TimeUnit;
  */
 public class DistributedLock implements Lock {
     private final String id;
-    private final Watcher watcher = Watcher.getInstance();
+    private final Watcher watcher;
 
+    private static final Logger logger = Logger.getLogger(Watcher.class.getName());
+
+    /**
+     * Constructor to create a new DistributedLock instance with a random UUID as the lock ID.
+     */
     public DistributedLock() {
         this.id = UUID.randomUUID().toString();
+        this.watcher = Watcher.getInstance();
         // Set event listener.
         this.watcher.addListener(new Watcher.LockEventListener(this));
     }
 
+    /**
+     * Constructor to create a new DistributedLock instance with a specified lock ID.
+     *
+     * @param idb Byte array representing the lock ID.
+     */
     public DistributedLock(byte[] idb) {
         this.id = new LockKey(idb).value();
+        this.watcher = Watcher.getInstance();
         // Set event listener.
         this.watcher.addListener(new Watcher.LockEventListener(this));
     }
 
+    public DistributedLock(String lockId) {
+        this(lockId.getBytes());
+    }
+
+    /**
+     * Acquires the lock. If the lock is not available, it waits until the lock is released.
+     */
     @Override
     public void lock() {
         // If try lock successfully, then the lock does exist, then don't need to lock.
         // And continue to work on the next steps.
-        if (!tryLock()) {
-            lock();
+        while (!tryLock()) {
+            logger.info("Waiting for lock to be released...");
         }
     }
 
+    /**
+     * Attempts to acquire the lock without waiting. Returns true if the lock was acquired successfully.
+     */
     @Override
     public boolean tryLock() {
         try {
             return tryLock(0L, TimeUnit.SECONDS);
         } catch (ApplicationException e) {
+            logger.severe("Error while attempting to lock: " + e.getMessage());
             throw new ApplicationRuntimeException(e.getMessage(), e.getCause());
         }
     }
 
+    /**
+     * Attempts to acquire the lock within the specified waiting time.
+     *
+     * @param timeout The maximum time to wait for the lock.
+     * @param unit    The time unit of the timeout parameter.
+     * @return True if the lock was acquired successfully within the specified time, false otherwise.
+     * @throws ApplicationException If an error occurs during lock acquisition.
+     */
     @Override
     public boolean tryLock(long timeout, TimeUnit unit) throws ApplicationException {
         // If the lock is existing, then wait for it to be released.
@@ -69,6 +102,7 @@ public class DistributedLock implements Lock {
                 else
                     watcher.waitFor(this.id);
             } catch (InterruptedException e) {
+                logger.severe("Error while waiting for lock: " + e.getMessage());
                 throw new ApplicationException(e.getMessage(), e.getCause());
             }
         } else {
@@ -76,10 +110,13 @@ public class DistributedLock implements Lock {
             this.watcher.register(this);
         }
 
-        // If you get this step, that means the lock has not been registered. and the thread can work on the next steps.
+        // If you get this step, that means the lock has not been registered, and the thread can work on the next steps.
         return true;
     }
 
+    /**
+     * Releases the lock.
+     */
     @Override
     public void unlock() {
         try {
@@ -87,35 +124,42 @@ public class DistributedLock implements Lock {
                 watcher.unregister(this);
             }
         } catch (ApplicationException e) {
+            logger.severe("Error while unlocking: " + e.getMessage());
             throw new ApplicationRuntimeException(e.getMessage(), e.getCause());
         }
     }
 
+    /**
+     * Gets the ID of the lock.
+     *
+     * @return The lock ID.
+     */
     @Override
     public String id() {
         return this.id;
     }
 
+    /**
+     * Checks if two DistributedLock objects are equal.
+     *
+     * @param obj The object to compare with.
+     * @return True if the objects are equal, false otherwise.
+     */
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
         DistributedLock other = (DistributedLock) obj;
-        if (id == null) {
-            return other.id == null;
-        } else return id.equals(other.id);
+        return Objects.equals(id, other.id);
     }
 
+    /**
+     * Calculates the hash code of the DistributedLock object.
+     *
+     * @return The hash code.
+     */
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+        return Objects.hash(id);
     }
-
 }
