f8217bdae7d81078f154c2ab042efec286921b1c
James ZHOU <moverinfo@gmail.com>
2024-01-18 11:41:36 +0800

    Update BuilderTest.java

--- src/test/java/org/tinystruct/data/component/BuilderTest.java
+++ src/test/java/org/tinystruct/data/component/BuilderTest.java
@@ -2,6 +2,10 @@ package org.tinystruct.data.component;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.tinystruct.ApplicationException;
+import org.tinystruct.system.util.TextFileLoader;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,10 +26,57 @@ public class BuilderTest {
         Assertions.assertDoesNotThrow(() -> {
             builder.parse(payload);
 
+            assertEquals("text-davinci-003", builder.get("model"));
+            assertEquals("", builder.get("prompt"));
             assertEquals(true, builder.get("correct"));
             assertEquals(false, builder.get("passed"));
             assertEquals(2500, builder.get("max_tokens"));
             assertEquals(0, builder.get("temperature"));
         });
+    }
+
+    @Test
+    void testBuilderToString() {
+        Builder builder = new Builder();
+        builder.put("name", "John Doe");
+        builder.put("age", 25);
+        builder.put("isStudent", true);
+
+        String expected = "{\"name\":\"John Doe\",\"age\":25,\"isStudent\":true}";
+        assertEquals(expected, builder.toString());
+    }
+
+    @Test
+    void testBuilderParse() throws ApplicationException {
+        String jsonString = "{\"name\":\"John Doe\",\"age\":25,\"isStudent\":true}";
+        Builder builder = new Builder();
+        builder.parse(jsonString);
+
+        assertEquals("John Doe", builder.get("name"));
+        assertEquals(25, builder.get("age"));
+        assertEquals(true, builder.get("isStudent"));
+    }
+
+    @Test
+    void testBuilderSaveAsFile() throws ApplicationException {
+        Builder builder = new Builder();
+        builder.put("name", "Jane Doe");
+        builder.put("age", 30);
+        builder.put("isStudent", false);
+
+        // Specify a file path where you want to save the data
+        String filePath = "src/test/testBuilderSaveAsFile.json";
+        builder.saveAsFile(new File(filePath));
+
+        // Load the saved file and parse it to check if the data is saved correctly
+        Builder loadedBuilder = new Builder();
+        loadedBuilder.parse(new TextFileLoader(new File(filePath)).getContent().toString());
+
+        assertEquals("Jane Doe", loadedBuilder.get("name"));
+        assertEquals(30, loadedBuilder.get("age"));
+        assertEquals(false, loadedBuilder.get("isStudent"));
+
+        // Clean up: Delete the created file
+        new File(filePath).delete();
     }
 }
