59856f1d44d3827f07d12824fde187e552b66e4d
James ZHOU <moverinfo@gmail.com>
2024-02-11 21:08:30 +0800

    Load commands template from the resource folder.

--- src/main/java/org/tinystruct/system/ApplicationManager.java
+++ src/main/java/org/tinystruct/system/ApplicationManager.java
@@ -8,10 +8,12 @@ import org.tinystruct.application.Action;
 import org.tinystruct.application.ActionRegistry;
 import org.tinystruct.application.Context;
 import org.tinystruct.system.cli.CommandLine;
+import org.tinystruct.system.util.TextFileLoader;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -100,223 +102,26 @@ public final class ApplicationManager {
                     Files.createDirectories(path);
                 }
 
-                String cmd;
-                if (Platform.isWindows()) {
-                    cmd = "@echo off\n" +
-                            "\n" +
-                            "@REM Check if JAVA_HOME is set and valid\n" +
-                            "if \"%JAVA_HOME%\" == \"\" (\n" +
-                            "    echo Error: JAVA_HOME not found in your environment. >&2\n" +
-                            "    echo Please set the JAVA_HOME variable in your environment to match the location of your Java installation. >&2\n" +
-                            "    exit /B 1\n" +
-                            ")\n" +
-                            "\n" +
-                            "if not exist \"%JAVA_HOME%\\bin\\java.exe\" (\n" +
-                            "    echo Error: JAVA_HOME is set to an invalid directory. >&2\n" +
-                            "    echo JAVA_HOME = \"%JAVA_HOME%\" >&2\n" +
-                            "    echo Please set the JAVA_HOME variable in your environment to match the location of your Java installation. >&2\n" +
-                            "    exit /B 1\n" +
-                            ")\n" +
-                            "\n" +
-                            "set \"JAVA_CMD=%JAVA_HOME%\\bin\\java.exe\"\n" +
-                            "\n" +
-                            "@REM Consolidate classpath entries, initialize ROOT and VERSION\n" +
-                            "set \"ROOT=%~dp0..\\\"\n" +
-                            "set \"VERSION=" + version + "\"\n" +
-                            "set \"classpath=%ROOT%target\\classes;%ROOT%lib\\tinystruct-%VERSION%-jar-with-dependencies.jar;%ROOT%lib\\*;%ROOT%WEB-INF\\lib\\*;%ROOT%WEB-INF\\classes;%USERPROFILE%\\.m2\\repository\\org\\tinystruct\\tinystruct\\%VERSION%\\tinystruct-%VERSION%-jar-with-dependencies.jar\"\n" +
-                            "\n" +
-                            "@REM Run Java application\n" +
-                            "%JAVA_CMD% -cp \"%classpath%\" org.tinystruct.system.Dispatcher %*";
-                } else {
-                    cmd = "#!/usr/bin/env sh\n" +
-                            "ROOT=\"$(pwd)\"\n" +
-                            "VERSION=\"" + version + "\"\n" +
-                            "cd \"$(dirname \"$0\")\" || exit\n" +
-                            "cd \"../\"\n" +
-                            "# Navigate to the root directory\n" +
-                            "cd \"$ROOT\" || exit\n" +
-                            "\n" +
-                            "# Java options initialization\n" +
-                            "JAVA_OPTS=\"\"\n" +
-                            "\n" +
-                            "# Arguments initialization\n" +
-                            "args=\"\"\n" +
-                            "\n" +
-                            "# Loop through each argument\n" +
-                            "for arg; do\n" +
-                            "    # Extract the first two characters of the argument\n" +
-                            "    str=${arg:0:2}\n" +
-                            "\n" +
-                            "    # Check if it starts with '-D' or '-X'\n" +
-                            "    if [ \"$str\" = \"-D\" ] || [ \"$str\" = \"-X\" ]; then\n" +
-                            "        JAVA_OPTS=\"$JAVA_OPTS $arg\"\n" +
-                            "    else\n" +
-                            "        args=\"$args $arg\"\n" +
-                            "    fi\n" +
-                            "done\n" +
-                            "\n" +
-                            "# Check if the JAR file exists\n" +
-                            "JAR_FILE=\"$ROOT/lib/tinystruct-$VERSION-jar-with-dependencies.jar\"\n" +
-                            "[ -f \"$JAR_FILE\" ] && JAR_FILE=\"$JAR_FILE:\"\n" +
-                            "\n" +
-                            "# Check if additional JAR files exist and add them to the classpath\n" +
-                            "# shellcheck disable=SC2043\n" +
-                            "for jar_file in \"$ROOT\"/lib/tinystruct-\"$VERSION\".jar; do\n" +
-                            "    [ -f \"$jar_file\" ] && JAR_FILE=\"$JAR_FILE$jar_file:\"\n" +
-                            "done\n" +
-                            "\n" +
-                            "# OS specific support.  $var _must_ be set to either true or false.\n" +
-                            "cygwin=false;\n" +
-                            "darwin=false;\n" +
-                            "mingw=false\n" +
-                            "case \"`uname`\" in\n" +
-                            "  CYGWIN*) cygwin=true ;;\n" +
-                            "  MINGW*) mingw=true;;\n" +
-                            "  Darwin*) darwin=true\n" +
-                            "    # Use /usr/libexec/java_home if available, otherwise fall back to /Library/Java/Home\n" +
-                            "    # See https://developer.apple.com/library/mac/qa/qa1170/_index.html\n" +
-                            "    if [ -z \"$JAVA_HOME\" ]; then\n" +
-                            "      if [ -x \"/usr/libexec/java_home\" ]; then\n" +
-                            "        export JAVA_HOME=\"`/usr/libexec/java_home`\"\n" +
-                            "      else\n" +
-                            "        export JAVA_HOME=\"/Library/Java/Home\"\n" +
-                            "      fi\n" +
-                            "    fi\n" +
-                            "    ;;\n" +
-                            "esac\n" +
-                            "\n" +
-                            "if [ -z \"$JAVA_HOME\" ] ; then\n" +
-                            "  if [ -r /etc/gentoo-release ] ; then\n" +
-                            "    JAVA_HOME=`java-config --jre-home`\n" +
-                            "  fi\n" +
-                            "fi\n" +
-                            "\n" +
-                            "if [ -z \"$M2_HOME\" ] ; then\n" +
-                            "  ## resolve links - $0 may be a link to maven's home\n" +
-                            "  PRG=\"$0\"\n" +
-                            "\n" +
-                            "  # need this for relative symlinks\n" +
-                            "  while [ -h \"$PRG\" ] ; do\n" +
-                            "    ls=`ls -ld \"$PRG\"`\n" +
-                            "    link=`expr \"$ls\" : '.*-> \\(.*\\)$'`\n" +
-                            "    if expr \"$link\" : '/.*' > /dev/null; then\n" +
-                            "      PRG=\"$link\"\n" +
-                            "    else\n" +
-                            "      PRG=\"`dirname \"$PRG\"`/$link\"\n" +
-                            "    fi\n" +
-                            "  done\n" +
-                            "\n" +
-                            "  saveddir=`pwd`\n" +
-                            "\n" +
-                            "  M2_HOME=`dirname \"$PRG\"`/..\n" +
-                            "\n" +
-                            "  # make it fully qualified\n" +
-                            "  M2_HOME=`cd \"$M2_HOME\" && pwd`\n" +
-                            "\n" +
-                            "  cd \"$saveddir\"\n" +
-                            "  # echo Using m2 at $M2_HOME\n" +
-                            "fi\n" +
-                            "\n" +
-                            "# For Cygwin, ensure paths are in UNIX format before anything is touched\n" +
-                            "if $cygwin ; then\n" +
-                            "  [ -n \"$M2_HOME\" ] &&\n" +
-                            "    M2_HOME=`cygpath --unix \"$M2_HOME\"`\n" +
-                            "  [ -n \"$JAVA_HOME\" ] &&\n" +
-                            "    JAVA_HOME=`cygpath --unix \"$JAVA_HOME\"`\n" +
-                            "  [ -n \"$CLASSPATH\" ] &&\n" +
-                            "    CLASSPATH=`cygpath --path --unix \"$CLASSPATH\"`\n" +
-                            "fi\n" +
-                            "\n" +
-                            "# For Migwn, ensure paths are in UNIX format before anything is touched\n" +
-                            "if $mingw ; then\n" +
-                            "  [ -n \"$M2_HOME\" ] &&\n" +
-                            "    M2_HOME=\"`(cd \"$M2_HOME\"; pwd)`\"\n" +
-                            "  [ -n \"$JAVA_HOME\" ] &&\n" +
-                            "    JAVA_HOME=\"`(cd \"$JAVA_HOME\"; pwd)`\"\n" +
-                            "  # TODO classpath?\n" +
-                            "fi\n" +
-                            "\n" +
-                            "if [ -z \"$JAVA_HOME\" ]; then\n" +
-                            "  javaExecutable=\"`which javac`\"\n" +
-                            "  if [ -n \"$javaExecutable\" ] && ! [ \"`expr \\\"$javaExecutable\\\" : '\\([^ ]*\\)'`\" = \"no\" ]; then\n" +
-                            "    # readlink(1) is not available as standard on Solaris 10.\n" +
-                            "    readLink=`which readlink`\n" +
-                            "    if [ ! `expr \"$readLink\" : '\\([^ ]*\\)'` = \"no\" ]; then\n" +
-                            "      if $darwin ; then\n" +
-                            "        javaHome=\"`dirname \\\"$javaExecutable\\\"`\"\n" +
-                            "        javaExecutable=\"`cd \\\"$javaHome\\\" && pwd -P`/javac\"\n" +
-                            "      else\n" +
-                            "        javaExecutable=\"`readlink -f \\\"$javaExecutable\\\"`\"\n" +
-                            "      fi\n" +
-                            "      javaHome=\"`dirname \\\"$javaExecutable\\\"`\"\n" +
-                            "      javaHome=`expr \"$javaHome\" : '\\(.*\\)/bin'`\n" +
-                            "      JAVA_HOME=\"$javaHome\"\n" +
-                            "      export JAVA_HOME\n" +
-                            "    fi\n" +
-                            "  fi\n" +
-                            "fi\n" +
-                            "\n" +
-                            "if [ -z \"$JAVACMD\" ] ; then\n" +
-                            "  if [ -n \"$JAVA_HOME\"  ] ; then\n" +
-                            "    if [ -x \"$JAVA_HOME/jre/sh/java\" ] ; then\n" +
-                            "      # IBM's JDK on AIX uses strange locations for the executables\n" +
-                            "      JAVACMD=\"$JAVA_HOME/jre/sh/java\"\n" +
-                            "    else\n" +
-                            "      JAVACMD=\"$JAVA_HOME/bin/java\"\n" +
-                            "    fi\n" +
-                            "  else\n" +
-                            "    JAVACMD=\"`which java`\"\n" +
-                            "  fi\n" +
-                            "fi\n" +
-                            "\n" +
-                            "if [ ! -x \"$JAVACMD\" ] ; then\n" +
-                            "  echo \"Error: JAVA_HOME is not defined correctly.\" >&2\n" +
-                            "  echo \"  We cannot execute $JAVACMD\" >&2\n" +
-                            "  exit 1\n" +
-                            "fi\n" +
-                            "\n" +
-                            "if [ -z \"$JAVA_HOME\" ] ; then\n" +
-                            "  echo \"Warning: JAVA_HOME environment variable is not set.\"\n" +
-                            "fi\n" +
-                            "\n" +
-                            "# For Cygwin, switch paths to Windows format before running java\n" +
-                            "if $cygwin; then\n" +
-                            "  [ -n \"$M2_HOME\" ] &&\n" +
-                            "    M2_HOME=`cygpath --path --windows \"$M2_HOME\"`\n" +
-                            "  [ -n \"$JAVA_HOME\" ] &&\n" +
-                            "    JAVA_HOME=`cygpath --path --windows \"$JAVA_HOME\"`\n" +
-                            "  [ -n \"$CLASSPATH\" ] &&\n" +
-                            "    CLASSPATH=`cygpath --path --windows \"$CLASSPATH\"`\n" +
-                            "  [ -n \"$MAVEN_PROJECTBASEDIR\" ] &&\n" +
-                            "    MAVEN_PROJECTBASEDIR=`cygpath --path --windows \"$MAVEN_PROJECTBASEDIR\"`\n" +
-                            "fi\n" +
-                            "\n" +
-                            "# Check if M2_HOME is not set or is equal to the current project path\n" +
-                            "if [ -z \"$M2_HOME\" ] || [ \"$M2_HOME\" = \"$(pwd)\" ]; then\n" +
-                            "    # Set M2_HOME to the .m2 folder under the user's home directory\n" +
-                            "    M2_HOME=\"$HOME/.m2\"\n" +
-                            "fi\n" +
-                            "\n" +
-                            "# Add all JAR files under the lib folder to the classpath\n" +
-                            "for jar_file in \"$ROOT\"/lib/*.jar; do\n" +
-                            "    CLASSPATH=\"$CLASSPATH:$jar_file\"\n" +
-                            "done\n" +
-                            "\n" +
-                            "# Java execution\n" +
-                            "$JAVACMD \\\n" +
-                            "$JAVA_OPTS \\\n" +
-                            "-XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/ \\\n" +
-                            "-cp \"$ROOT/target/classes:$JAR_FILE$CLASSPATH:$ROOT/WEB-INF/lib/*:$ROOT/WEB-INF/classes:$M2_HOME/repository/org/tinystruct/tinystruct/$VERSION/tinystruct-$VERSION-jar-with-dependencies.jar\" org.tinystruct.system.Dispatcher \"$@\"";
-                }
-
                 path = Paths.get(origin);
                 if (!Files.exists(path))
                     Files.createFile(path);
 
-                try (BufferedWriter writer = Files.newBufferedWriter(path)) {
-                    writer.write(cmd);
+                String cmd = null;
+                try (InputStream in = ApplicationManager.class.getResourceAsStream("dispatcher" + (Platform.isWindows() ? ".cmd" : ""))) {
+                    if (null != in) {
+                        TextFileLoader loader = new TextFileLoader(in);
+                        cmd = loader.getContent().toString();
+                        cmd = cmd.replaceAll("\\{%VERSION%}", version);
+                    }
+                } catch (IOException e) {
+                    throw new ApplicationRuntimeException(e.getMessage(), e);
                 }
 
+                if (cmd != null) {
+                    try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+                        writer.write(cmd.toString());
+                    }
+                }
             } catch (IOException e) {
                 throw new ApplicationException(e.getMessage(), e);
             }
