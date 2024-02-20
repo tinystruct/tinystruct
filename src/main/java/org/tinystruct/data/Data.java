cd8c3bb5fe652f5782a98a874b473c02fb116b24
James ZHOU <moverinfo@gmail.com>
2024-02-20 23:39:02 +0800

    Add code comments for functionalities.

--- src/main/java/org/tinystruct/data/Data.java
+++ src/main/java/org/tinystruct/data/Data.java
@@ -20,42 +20,128 @@ import org.tinystruct.data.component.Condition;
 import org.tinystruct.data.component.Row;
 import org.tinystruct.data.component.Table;
 
+/**
+ * This interface defines methods for data operations.
+ */
 public interface Data {
 
+    /**
+     * Get the class path of the data object.
+     */
     String getClassPath();
 
+    /**
+     * Get the class name of the data object.
+     */
     String getClassName();
 
+    /**
+     * Set the database table associated with the data object.
+     */
     void setTableName(String attribute);
 
+    /**
+     * Set the identifier of the data object.
+     */
     Object setId(Object Id);
 
+    /**
+     * Get the identifier of the data object.
+     */
     Object getId();
 
+    /**
+     * Append a new record to the database.
+     * @return true if the operation is successful, false otherwise.
+     * @throws ApplicationException if an application-specific error occurs.
+     */
     boolean append() throws ApplicationException;
 
+    /**
+     * Update an existing record in the database.
+     * @return true if the operation is successful, false otherwise.
+     * @throws ApplicationException if an application-specific error occurs.
+     */
     boolean update() throws ApplicationException;
 
+    /**
+     * Delete a record from the database.
+     * @return true if the operation is successful, false otherwise.
+     * @throws ApplicationException if an application-specific error occurs.
+     */
     boolean delete() throws ApplicationException;
 
+    /**
+     * Set the request fields for querying data.
+     */
     Data setRequestFields(String fields);
 
+    /**
+     * Set the order by clause for querying data.
+     */
     Data orderBy(String[] fieldNames);
 
+    /**
+     * Find records in the database based on the given SQL query and parameters.
+     * @param SQL the SQL query.
+     * @param parameters the parameters for the query.
+     * @return a Table containing the query result.
+     * @throws ApplicationException if an application-specific error occurs.
+     */
     Table find(String SQL, Object[] parameters) throws ApplicationException;
 
+    /**
+     * Find records in the database based on the given condition and parameters.
+     * @param condition the condition for the query.
+     * @param parameters the parameters for the query.
+     * @return a Table containing the query result.
+     * @throws ApplicationException if an application-specific error occurs.
+     */
     Table find(Condition condition, Object[] parameters) throws ApplicationException;
 
+    /**
+     * Find records in the database based on the given where clause and parameters.
+     * @param where the where clause for the query.
+     * @param parameters the parameters for the query.
+     * @return a Table containing the query result.
+     * @throws ApplicationException if an application-specific error occurs.
+     */
     Table findWith(String where, Object[] parameters) throws ApplicationException;
 
+    /**
+     * Find a single record in the database based on the given SQL query and parameters.
+     * @param SQL the SQL query.
+     * @param parameters the parameters for the query.
+     * @return a Row containing the query result.
+     * @throws ApplicationException if an application-specific error occurs.
+     */
     Row findOne(String SQL, Object[] parameters) throws ApplicationException;
 
+    /**
+     * Find a single record in the database by its identifier.
+     * @return a Row containing the query result.
+     * @throws ApplicationException if an application-specific error occurs.
+     */
     Row findOneById() throws ApplicationException;
 
+    /**
+     * Find a single record in the database by the given primary key and value.
+     * @param PK the primary key.
+     * @param value the value of the primary key.
+     * @return a Row containing the query result.
+     * @throws ApplicationException if an application-specific error occurs.
+     */
     Row findOneByKey(String PK, String value) throws ApplicationException;
 
+    /**
+     * Find all records in the database.
+     * @return a Table containing all records.
+     * @throws ApplicationException if an application-specific error occurs.
+     */
     Table findAll() throws ApplicationException;
 
+    /**
+     * Get the repository associated with the data object.
+     */
     Repository getRepository();
-
 }
--- src/main/java/org/tinystruct/data/component/AbstractData.java
+++ src/main/java/org/tinystruct/data/component/AbstractData.java
@@ -33,11 +33,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * @author James
+ * This class provides a skeletal implementation of the Data interface.
+ * It implements common methods for database operations and data manipulation.
  */
 public abstract class AbstractData implements Data {
 
+    // Logger for logging messages
     private final static Logger logger = Logger.getLogger(AbstractData.class.getName());
+
+    // Repository for database operations
     private static Repository repository;
 
     static {
@@ -48,13 +52,27 @@ public abstract class AbstractData implements Data {
         }
     }
 
+    // Identifier for the data
     protected Object Id;
+
+    // Class path of the data object
     private String classPath;
+
+    // Class name of the data object
     private String className;
+
+    // Database table associated with the data object
     private String table;
+
+    // Fields ready for database operations
     private Field readyFields;
+
+    // Condition for querying data
     private Condition condition;
 
+    /**
+     * Constructor to initialize classPath, className, and readyFields.
+     */
     public AbstractData() {
         this.className = this.getClass().getSimpleName();
 
@@ -65,12 +83,13 @@ public abstract class AbstractData implements Data {
         }
 
         try {
-            this.readyFields = Mapping.getMappedField(this);
+            this.readyFields = Mapping.getMappedField(this); // Get mapped fields of the data object
         } catch (ApplicationException e) {
             logger.log(Level.SEVERE, e.getMessage(), e);
         }
     }
 
+    // Method to get the default server based on the database driver specified in the properties file
     private static Repository getDefaultServer() throws ApplicationException {
         Configuration<String> properties = new Settings("/application.properties");
         String driver = properties.get("driver");
@@ -105,13 +124,19 @@ public abstract class AbstractData implements Data {
         return repository;
     }
 
+    /**
+     * Get the class path of the data object.
+     */
     @Override
-	public String getClassPath() {
+    public String getClassPath() {
         return this.classPath;
     }
 
+    /**
+     * Set the identifier of the data object.
+     */
     @Override
-	public Object setId(Object id) {
+    public Object setId(Object id) {
         this.Id = id;
 
         if (this.readyFields != null && this.readyFields.containsKey("Id")) {
@@ -182,35 +207,46 @@ public abstract class AbstractData implements Data {
         return false;
     }
 
+    /**
+     * Append a new record to the database.
+     */
     @Override
-	public boolean append() throws ApplicationException {
+    public boolean append() throws ApplicationException {
         return repository.append(this.readyFields, this.table);
     }
 
+    /**
+     * Update an existing record in the database.
+     */
     @Override
-	public boolean update() throws ApplicationException {
+    public boolean update() throws ApplicationException {
         return repository.update(readyFields, this.table);
     }
 
+    /**
+     * Delete a record from the database.
+     */
     @Override
-	public boolean delete() throws ApplicationException {
+    public boolean delete() throws ApplicationException {
         return repository.delete(this.Id, this.table);
     }
 
-    protected int countAll(String SQL) {
-        return 0;
-    }
-
+    /**
+     * Set the request fields for querying data.
+     */
     @Override
-	public Data setRequestFields(String fields) {
+    public Data setRequestFields(String fields) {
         this.condition = new Condition();
         this.condition.setRequestFields(fields);
 
         return this;
     }
 
+    /**
+     * Set the order by clause for querying data.
+     */
     @Override
-	public Data orderBy(String[] fieldNames) {
+    public Data orderBy(String[] fieldNames) {
         StringBuilder orders = new StringBuilder();
         for (String fields : fieldNames) {
             if (orders.length() > 0)
@@ -226,28 +262,43 @@ public abstract class AbstractData implements Data {
         return this;
     }
 
+    /**
+     * Find records in the database based on the given SQL query and parameters.
+     */
     @Override
-	public Table find(String SQL, Object[] parameters) throws ApplicationException {
+    public Table find(String SQL, Object[] parameters) throws ApplicationException {
         return repository.find(SQL, parameters);
     }
 
+    /**
+     * Find records in the database based on the given condition and parameters.
+     */
     @Override
-	public Table find(Condition condition, Object[] parameters) throws ApplicationException {
+    public Table find(Condition condition, Object[] parameters) throws ApplicationException {
         return this.find(condition.toString(), parameters);
     }
 
+    /**
+     * Find records in the database based on the given where clause and parameters.
+     */
     @Override
-	public Table findWith(String where, Object[] parameters) throws ApplicationException {
+    public Table findWith(String where, Object[] parameters) throws ApplicationException {
         return this.find(Objects.requireNonNullElseGet(this.condition, Condition::new).select(this.table).with(where), parameters);
     }
 
+    /**
+     * Find a single record in the database based on the given SQL query and parameters.
+     */
     @Override
-	public Row findOne(String SQL, Object[] parameters) throws ApplicationException {
+    public Row findOne(String SQL, Object[] parameters) throws ApplicationException {
         return repository.findOne(SQL, parameters);
     }
 
+    /**
+     * Find a single record in the database by its identifier.
+     */
     @Override
-	public Row findOneById() throws ApplicationException {
+    public Row findOneById() throws ApplicationException {
         Row row = this.findOne(new Condition().select(this.table).and(
                 "id=?").toString(), new Object[]{this.Id});
 
@@ -257,8 +308,11 @@ public abstract class AbstractData implements Data {
         return row;
     }
 
+    /**
+     * Find a single record in the database by the given primary key and value.
+     */
     @Override
-	public Row findOneByKey(String PK, String value) throws ApplicationException {
+    public Row findOneByKey(String PK, String value) throws ApplicationException {
         Row row = this.findOne(new Condition().select(this.table).and(
                 PK + "=?").toString(), new Object[]{value});
 
@@ -268,41 +322,60 @@ public abstract class AbstractData implements Data {
         return row;
     }
 
+    /**
+     * Find all records in the database.
+     */
     @Override
-	public Table findAll() throws ApplicationException {
+    public Table findAll() throws ApplicationException {
         return this.find(Objects.requireNonNullElseGet(this.condition, Condition::new).select(this.table), new Object[]{});
     }
 
+    /**
+     * Get the repository associated with the data object.
+     */
+    @Override
+    public Repository getRepository() {
+        return repository;
+    }
+
+    // Abstract method to be implemented by subclasses
     public abstract void setData(Row row);
 
+    /**
+     * Get the class name of the data object.
+     */
     @Override
-	public abstract String toString();
-
-    @Override
-	public String getClassName() {
+    public String getClassName() {
         return className;
     }
 
+    /**
+     * Set the class name of the data object.
+     */
     protected void setClassName(String className) {
         this.className = className;
     }
 
+    /**
+     * Get the identifier of the data object.
+     */
     @Override
-	public Object getId() {
+    public Object getId() {
         return this.Id;
     }
 
+    /**
+     * Get the database table associated with the data object.
+     */
     public String getTableName() {
         return this.table;
     }
 
+    /**
+     * Set the database table associated with the data object.
+     */
     @Override
-	public void setTableName(String table) {
+    public void setTableName(String table) {
         this.table = table;
-    }
-
-    @Override
-	public Repository getRepository() {
-        return repository;
     }
 }
