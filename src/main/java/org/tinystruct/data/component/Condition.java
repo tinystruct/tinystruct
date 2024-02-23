b5c8d790f73cd215fcfc78be58c8f193a859c8b0
James ZHOU <moverinfo@gmail.com>
2024-02-23 23:09:40 +0800

    Add comments for code.

--- src/main/java/org/tinystruct/data/component/Condition.java
+++ src/main/java/org/tinystruct/data/component/Condition.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+ /*******************************************************************************
  * Copyright  (c) 2013, 2023 James Mover Zhou
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,10 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-package org.tinystruct.data.component;
+ package org.tinystruct.data.component;
 
-import java.io.Serializable;
+ import java.io.Serializable;
 
+ /**
+ * Represents a SQL condition for building dynamic SQL queries.
+ */
 public class Condition implements Serializable {
 
     private static final long serialVersionUID = 7375286601225137163L;
@@ -25,12 +28,21 @@ public class Condition implements Serializable {
     private String fields;
     private String orders;
 
+    /**
+     * Constructs a new Condition object with default settings.
+     */
     public Condition() {
         this.SQL = new StringBuilder();
         this.fields = "*";
         this.orders = "";
     }
 
+    /**
+     * Sets the table for the SQL query.
+     *
+     * @param table The table name.
+     * @return The Condition object.
+     */
     public Condition select(String table) {
         this.table = table;
         this.SQL = new StringBuilder();
@@ -38,6 +50,12 @@ public class Condition implements Serializable {
         return this;
     }
 
+    /**
+     * Adds an AND condition to the SQL query.
+     *
+     * @param condition The condition to add.
+     * @return The Condition object.
+     */
     public Condition and(String condition) {
         String query = this.SQL.toString();
         if (query.toUpperCase().contains("WHERE"))
@@ -48,6 +66,12 @@ public class Condition implements Serializable {
         return this;
     }
 
+    /**
+     * Adds an OR condition to the SQL query.
+     *
+     * @param condition The condition to add.
+     * @return The Condition object.
+     */
     public Condition or(String condition) {
         String query = this.SQL.toString();
         if (query.toUpperCase().contains("WHERE"))
@@ -58,6 +82,12 @@ public class Condition implements Serializable {
         return this;
     }
 
+    /**
+     * Sets the order for the SQL query.
+     *
+     * @param orders The order clause.
+     * @return The Condition object.
+     */
     public Condition orderBy(String orders) {
         this.orders = " order by " + orders.toLowerCase();
         String query = this.SQL.toString();
@@ -67,20 +97,36 @@ public class Condition implements Serializable {
         return this;
     }
 
+    /**
+     * Constructs a new SQL query using custom SQL.
+     *
+     * @param sql The custom SQL string.
+     * @return The Condition object.
+     */
     public Condition with(String sql) {
         this.SQL = new StringBuilder();
         this.SQL.append("SELECT ").append(this.fields).append(" FROM ").append(this.table).append(" ").append(sql).append(this.orders);
         return this;
     }
 
+    /**
+     * Sets the fields to be retrieved in the SQL query.
+     *
+     * @param fields The fields to retrieve.
+     * @return The Condition object.
+     */
     public Condition setRequestFields(String fields) {
         this.fields = fields;
         return this;
     }
 
+    /**
+     * Converts the Condition object to a string representation of the SQL query.
+     *
+     * @return The SQL query string.
+     */
     @Override
-	public String toString() {
+    public String toString() {
         return this.SQL.toString();
     }
-
 }
