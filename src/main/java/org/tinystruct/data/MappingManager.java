ec71400565026616c392d398e0dc1b0e78044e79
James ZHOU <moverinfo@gmail.com>
2024-03-16 23:40:01 +0800

    Add code comment blocks.

--- src/main/java/org/tinystruct/data/MappingManager.java
+++ src/main/java/org/tinystruct/data/MappingManager.java
@@ -16,38 +16,69 @@
 package org.tinystruct.data;
 
 import org.tinystruct.dom.Document;
-
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * A singleton class for managing a mapping of string keys to Document objects.
+ */
 public final class MappingManager {
+    // The underlying ConcurrentHashMap for storing the mapping
     private final ConcurrentHashMap<String, Document> map;
 
+    // Private constructor to prevent external instantiation
     private MappingManager() {
         this.map = new ConcurrentHashMap<String, Document>();
     }
 
+    /**
+     * Returns the singleton instance of MappingManager.
+     *
+     * @return The singleton instance of MappingManager
+     */
     public static MappingManager getInstance() {
         return SingletonHolder.manager;
     }
 
+    /**
+     * Sets a key-value pair in the mapping.
+     *
+     * @param name  The key
+     * @param value The Document object to associate with the key
+     */
     public void set(String name, Document value) {
         map.putIfAbsent(name, value);
     }
 
+    /**
+     * Retrieves the Document object associated with the specified key.
+     *
+     * @param name The key to retrieve the value for
+     * @return The Document object associated with the key, or null if not found
+     */
     public Document get(String name) {
         return map.get(name);
     }
 
+    /**
+     * Removes the entry with the specified key from the mapping.
+     *
+     * @param name The key of the entry to remove
+     */
     public void remove(String name) {
         map.remove(name);
     }
 
+    /**
+     * Returns the number of entries in the mapping.
+     *
+     * @return The number of entries in the mapping
+     */
     public int size() {
         return map.size();
     }
 
+    // Private static inner class for lazy initialization of the singleton instance
     private static final class SingletonHolder {
         static final MappingManager manager = new MappingManager();
     }
-
 }
