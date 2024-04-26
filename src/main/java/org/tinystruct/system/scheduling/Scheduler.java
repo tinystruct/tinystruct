7eabc37507b6c28fa2030c22cd7a31a0faa210f7
James ZHOU <moverinfo@gmail.com>
2024-04-26 19:35:24 +0800

    Add code comment blocks.

--- src/main/java/org/tinystruct/system/scheduling/Scheduler.java
+++ src/main/java/org/tinystruct/system/scheduling/Scheduler.java
@@ -19,23 +19,42 @@ import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * The Scheduler class provides a way to schedule tasks at specific times or intervals.
+ */
 public class Scheduler {
 
     private final static Scheduler INSTANCE = new Scheduler(true);
     private final Timer timer;
 
+    /**
+     * Constructs a Scheduler instance.
+     * @param isDaemon Whether the Timer associated with this Scheduler is a daemon thread.
+     */
     public Scheduler(boolean isDaemon) {
         this.timer = new Timer(isDaemon);
     }
 
+    /**
+     * Retrieves the singleton instance of the Scheduler.
+     * @return The Scheduler instance.
+     */
     public static Scheduler getInstance() {
         return INSTANCE;
     }
 
+    /**
+     * Cancels the Timer associated with this Scheduler.
+     */
     public void cancel() {
         timer.cancel();
     }
 
+    /**
+     * Schedules a task to be executed at specific times based on the provided ScheduleIterator.
+     * @param schedulerTask The task to be scheduled.
+     * @param iterator The ScheduleIterator defining the times for task execution.
+     */
     public void schedule(final SchedulerTask schedulerTask, final ScheduleIterator iterator) {
         synchronized (schedulerTask.lock) {
             final Scheduler scheduler = this;
@@ -43,9 +62,8 @@ public class Scheduler {
             Date time = iterator.getTime();
             if (time == null) {
                 schedulerTask.cancel();
-            } else
+            } else {
                 timer.schedule(new TimerTask() {
-
                     @Override
                     public void run() {
                         schedulerTask.start();
@@ -54,15 +72,25 @@ public class Scheduler {
                             scheduler.schedule(schedulerTask, iterator.next());
                         }
                     }
-
                 }, time);
+            }
         }
     }
 
+    /**
+     * Schedules a task to be executed at fixed-rate intervals.
+     * @param task The task to be scheduled.
+     * @param iterator The ScheduleIterator defining the initial execution time.
+     * @param period The interval between successive task executions, in milliseconds.
+     */
     public void schedule(final TimerTask task, final ScheduleIterator iterator, long period) {
         this.timer.scheduleAtFixedRate(task, iterator.getTime(), period);
     }
 
+    /**
+     * Removes a previously scheduled task from execution.
+     * @param task The task to be removed.
+     */
     public void remove(final TimerTask task) {
         task.cancel();
     }
