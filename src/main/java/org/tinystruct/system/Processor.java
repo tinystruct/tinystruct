050766490b62f6dbb7871f96365d09a0c1acf5ab
James ZHOU <moverinfo@gmail.com>
2024-07-11 23:42:19 +0800

    Add Processor interface.

---
+++ src/main/java/org/tinystruct/system/Processor.java
@@ -0,0 +1,17 @@
+package org.tinystruct.system;
+
+/**
+ * Interface for defining processors that operate on a specific type of input.
+ *
+ * @param <T> the type of input data that the processor operates on and returns
+ */
+public interface Processor<T> {
+
+    /**
+     * Processes the input data and returns the processed result.
+     *
+     * @param input the input data to process
+     * @return the processed result of type T
+     */
+    T process(T input);
+}
