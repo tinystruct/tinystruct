972cc6613bd3fed4a3cf68fd3005aa2ac8a726b8
@James <moverinfo@gmail.com>
2024-08-12 23:46:24 +0800

    Add VectorOperator.

---
+++ src/main/java/org/tinystruct/data/VectorOperator.java
@@ -0,0 +1,9 @@
+package org.tinystruct.data;
+
+import org.tinystruct.ApplicationException;
+
+public class VectorOperator extends DatabaseOperator {
+    public VectorOperator() throws ApplicationException {
+        super();
+    }
+}
