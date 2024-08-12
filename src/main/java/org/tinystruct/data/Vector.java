ba5dbf2efc277a29c3a0946ea20cb607d8282721
@James <moverinfo@gmail.com>
2024-08-12 23:44:46 +0800

    Add Vector interface.

--- src/main/java/org/tinystruct/data/Vector.java
+++ src/main/java/org/tinystruct/data/Vector.java
@@ -1,5 +1,8 @@
 package org.tinystruct.data;
 
+import org.tinystruct.ApplicationException;
+import org.tinystruct.data.component.SearchResult;
+
 import java.util.List;
 
 /**
@@ -9,19 +12,24 @@ import java.util.List;
 public interface Vector {
 
     /**
-     * Adds a list of Data objects to the collection.
+     * Adds a vector to the database with the associated label, entity ID, and entity type.
      *
-     * @param documents the list of Data objects to be added
+     * @param vector     The vector to add, represented as a double array.
+     * @param label      The label associated with the vector.
+     * @param entityId   The ID of the entity associated with the vector.
+     * @param entityType The type of the entity associated with the vector.
+     * @throws ApplicationException If an error occurs while adding the vector to the database.
      */
-    void add(List<Data> documents);
+    void add(double[] vector, String label, int entityId, String entityType) throws ApplicationException;
 
     /**
-     * Deletes Data objects from the collection based on their identifiers.
+     * Deletes vectors from the database based on the provided list of vector IDs.
      *
-     * @param ids the list of identifiers of the Data objects to be deleted
-     * @return true if the deletion was successful, false otherwise
+     * @param ids A list of vector IDs to delete.
+     * @return True if the deletion was successful, false otherwise.
+     * @throws ApplicationException If an error occurs while deleting vectors from the database.
      */
-    boolean delete(List<String> ids);
+    boolean delete(List<String> ids) throws ApplicationException;
 
     /**
      * Finds and returns a list of Data objects that are similar to the given query.
@@ -30,4 +38,15 @@ public interface Vector {
      * @return a list of Data objects that are similar to the query
      */
     List<Data> similarity(String query);
+
+    /**
+     * Searches the database for vectors similar to the provided query vector.
+     * Returns the top K most similar vectors based on cosine similarity.
+     *
+     * @param queryVector The vector to search for, represented as a double array.
+     * @param topK        The number of top similar vectors to return.
+     * @return A list of SearchResult objects containing the top K similar vectors.
+     * @throws ApplicationException If an error occurs while searching the database.
+     */
+    List<SearchResult> search(double[] queryVector, int topK) throws ApplicationException;
 }
