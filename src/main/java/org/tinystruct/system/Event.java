160a2d48f426e02673b0f06bc6492237a69f5bc3
@James <moverinfo@gmail.com>
2024-10-28 20:51:32 +0800

    Improve Event interface.

--- src/main/java/org/tinystruct/system/Event.java
+++ src/main/java/org/tinystruct/system/Event.java
@@ -1,4 +1,24 @@
 package org.tinystruct.system;
 
-public interface Event {
+/**
+ * Represents a generic event in the system.
+ *
+ * @param <T> the type of the payload associated with the event.
+ */
+public interface Event<T> {
+    /**
+     * Gets the name of the event.
+     * This can be used to identify or categorize the event.
+     *
+     * @return the name of the event as a String.
+     */
+    String getName();
+
+    /**
+     * Gets the payload of the event.
+     * The payload contains any additional data associated with the event.
+     *
+     * @return the payload of the event, which can be of any type specified by T.
+     */
+    T getPayload();
 }
