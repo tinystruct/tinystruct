e25cf5383c24e768031d009fa22b1aeec6557725
@James <moverinfo@gmail.com>
2024-11-01 23:48:59 +0800

    Update MQTTClientTest.java

--- src/test/java/org/tinystruct/mqtt/MQTTClientTest.java
+++ src/test/java/org/tinystruct/mqtt/MQTTClientTest.java
@@ -12,61 +12,28 @@ class MQTTClientTest {
     @Test
     void publish() {
         subscribe();
-        client.publish("test", "hello");
-        client.publish("test", "Praise to the Lord!");
+        client.publish("@test@", "Praise to the Lord!");
     }
 
     @Test
     void subscribe() {
-        client.subscribe("test", new MessageListener() {
+        client.subscribe("@test@", new MessageListener<Event>() {
             @Override
             public void on(Event event, Callback callback) {
-                callback.process();
+                callback.process(event);
             }
 
             @Override
-            public void onMessage(final String topic, Object message) {
-                this.on(new Event() {
-                    /**
-                     * Gets the name of the event.
-                     * This can be used to identify or categorize the event.
-                     *
-                     * @return the name of the event as a String.
-                     */
-                    @Override
-                    public String getName() {
-                        return "test";
-                    }
-
-                    /**
-                     * Gets the payload of the event.
-                     * The payload contains any additional data associated with the event.
-                     *
-                     * @return the payload of the event, which can be of any type specified by T.
-                     */
-                    @Override
-                    public Object getPayload() {
-                        return "{}";
-                    }
-                }, new MessageCallback(message) {
-                    @Override
-                    public void process() {
-                        if(topic.equalsIgnoreCase("test")) {
-                            System.out.println("OK:" + message);
+            public void onMessage(final String topic, Event evt) {
+                if (topic.equalsIgnoreCase("@test@")) {
+                    this.on(evt, new Callback<Event>() {
+                        @Override
+                        public void process(Event evt) {
+                            System.out.println("OK:" + evt.getPayload());
                         }
-                    }
-                });
+                    });
+                }
             }
         });
-    }
-
-    class MessageCallback implements Callback {
-        Object message;
-        public MessageCallback(Object message) {
-            this.message = message;
-        }
-
-        public void process() {
-        }
     }
 }
