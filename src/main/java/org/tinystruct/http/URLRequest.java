5a5cd9bd8188d7f47c07a2599f928e76c6b8a12b
@James <moverinfo@gmail.com>
2025-02-01 16:32:08 +0800

    Update the date of copyright.

--- src/main/java/org/tinystruct/http/URLRequest.java
+++ src/main/java/org/tinystruct/http/URLRequest.java
@@ -1,7 +1,7 @@
 package org.tinystruct.http;
 
 /*******************************************************************************
- * Copyright  (c) 2013, 2023 James Mover Zhou
+ * Copyright  (c) 2013, 2025 James M. ZHOU
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/main/java/org/tinystruct/system/scheduling/Scheduler.java
+++ src/main/java/org/tinystruct/system/scheduling/Scheduler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright  (c) 2013, 2023 James Mover Zhou
+ * Copyright  (c) 2013, 2025 James M. ZHOU
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
