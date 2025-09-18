28a9a4f403247f9e22dfd22fee788bccd82ba64f
@James <moverinfo@gmail.com>
2025-09-18 23:52:58 +0800

    Move out tomcat server, netty http server, mqtt and redis support to separated projects.

--- src/main/java/org/tinystruct/mqtt/MessageListener.java
+++
@@ -1,7 +0,0 @@
-package org.tinystruct.mqtt;
-
-import org.tinystruct.system.Listener;
-
-public interface MessageListener<T> extends Listener<T> {
-    void onMessage(String topic, T message);
-}
--- src/main/java/org/tinystruct/mqtt/MessageQueue.java
+++
@@ -1,7 +0,0 @@
-package org.tinystruct.mqtt;
-
-public interface MessageQueue<T> {
-    void subscribe(String topic, MessageListener<T> listener);
-
-    void publish(String topic, String message);
-}
