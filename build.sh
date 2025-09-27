8669ddee9ec327c51da2bacae79a4ee36418198e
@James <moverinfo@gmail.com>
2025-09-27 21:09:29 +0800

    Update the version to be 1.7.7.

--- build.sh
+++ build.sh
@@ -5,14 +5,9 @@ set -ex
 source ~/.bash_profile
 
 $GRAALVM_HOME/bin/native-image -H:ConfigurationFileDirectories=/Volumes/Untitled/development/tinystruct/bin/.metadata --no-fallback \
---initialize-at-run-time=org.slf4j.LoggerFactory \
---initialize-at-run-time=io.netty.handler.ssl.BouncyCastleAlpnSslUtils \
---initialize-at-run-time=io.netty.util.AbstractReferenceCounted \
---trace-class-initialization=org.slf4j.LoggerFactory \
 --add-opens=java.base/jdk.internal.misc=ALL-UNNAMED \
--cp ./target/tinystruct-1.7.6.jar \
+-cp ./target/tinystruct-1.7.7.jar \
 -H:Name=dispatcher-native \
 -H:Class=org.tinystruct.system.Dispatcher \
 -H:+ReportExceptionStackTraces \
--Dio.netty.tryReflectionSetAccessible=true \
 -H:+ReportUnsupportedElementsAtRuntime
